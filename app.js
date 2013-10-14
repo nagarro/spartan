22b0c7256182c0a4a2bdb779e5441a2bcd98f2e4
Sumeet Malik <sumeet2113@nagarro.com>
2013-10-14 19:03:54 +0530

    Integrated handsontable
    integrated a jquery plugin for grid to backbone mtv

---
+++ app.js
@@ -0,0 +1,54 @@
+// View for displaying users.
+define(function(require){
+	return {
+		initialize: function () {
+			var Backbone = require('backbone');
+			var AppRouter = Backbone.Router.extend({
+				routes: {
+					"*others" : "defaultRoute"
+				},
+				defaultRoute: function(){
+					var UserModel, UserCollection, UserView, userModel, userCollection, userView;
+					
+					UserModel = require('models/user');
+					UserCollection = require('models/users');
+					UserView = require('views/users');
+					
+					userCollection = new UserCollection();
+					userModel = new UserModel({
+									id: '1',
+									Name: 'Dheeraj Lalwani',
+									Age: 27,
+									IsSingle: true
+								});
+					userCollection.add(userModel);
+					
+					userModel = new UserModel({
+									id: '2',
+									Name: 'Sumeet Malik',
+									Age: 26,
+									IsSingle: true
+								});
+					userCollection.add(userModel);
+					
+					userModel = new UserModel({
+									id: '3',
+									Name: 'Anup Bishnoi',
+									Age: 28,
+									IsSingle: false
+								});
+					userCollection.add(userModel);
+
+					userView = new UserView({
+								el: '#container',
+								model: userCollection
+							  });
+					userView.render();
+				}
+			});
+
+			var myAppRouter = new AppRouter();
+			Backbone.history.start();
+		}
+	}; 
+});
